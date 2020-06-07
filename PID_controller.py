f70461599aa3e1b599b642e54460d4e129dddcaf
mironoff2007 <mironoff2007@rambler.ru>
2020-06-07 22:42:21 +0300

    Refactored
    PID logic moved to separate class

---
+++ PID_controller.py
@@ -0,0 +1,63 @@
+class PID_controller:
+
+    def __init__(self,Kp,Ki,Kd):
+        self.t_last = 0.0
+        self.val = 0
+        self.tar = 0
+        self.err = 0
+        self.err_last = 0
+        self.err_sum = 0
+        self.err_last = 0
+        self.d_err = 0
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+
+    def getControl(self,val, tar,t):
+        Kp=self.Kp
+        Ki = self.Ki
+        Kd = self.Kd
+        t_last=self.t_last
+        err_sum = self.err_sum
+        err_last=self.err_last
+        #
+
+        #delta time
+        dt = t-t_last
+        #error
+        err = tar - val
+
+        #error sum, integral
+        err_sum= err_sum + err*dt
+        #cut-off value
+        if Ki!=0:
+            err_sum_cutoff=1/Ki
+        else:
+            err_sum_cutoff=1
+
+        # integral cut-off
+        if err_sum > err_sum_cutoff:
+            err_sum = err_sum_cutoff
+        elif err_sum < -err_sum_cutoff:
+            err_sum = -err_sum_cutoff
+
+        #error derivative
+        if dt==0 :
+            d_err=0
+        else:
+            d_err = (err - err_last)/dt
+
+        #output control
+        y=Kp * err + Ki * err_sum + Kd * d_err
+
+        #output control cut-off
+        if y > 1:
+            y = 1
+        elif y < -1:
+            y = -1
+
+        self.err_last = err_last
+        self.err_sum = err_sum
+        self.t_last=t
+
+        return y
--- main.py
+++ main.py
@@ -1,59 +1,35 @@
 import graph as gr
-x0=0;
-x=0;
+from PID_controller import PID_controller
+
+x=0
 vel=0
 ar=[]
 f=[]
 speed=[]
 t=[]
-dx=0
-dSum=0
-dSum_lim=20
-a=2
-y_max=1;
-v_max=10
-dt=1
-for i in range (0,500):
-    if i<20 :
+dx_arr=[]
+dSum_arr=[]
+
+
+a=0
+
+dt=0.05
+timer=0.0
+controller=PID_controller(0.8,0.5,0)
+
+for i in range(0, 2000):
+    if i < 20:
         ar.append(0)
     else:
-        ar.append(100)
+        ar.append(50)
+        timer=timer+dt
+#control
 
-    dx=ar[i]-x
-    if dx >    10:
-        dx = 10
-    elif dx < -10:
-        dx = -10
+    y=controller.getControl(x,ar[i],timer)
 
-    print(dSum)
-    dSum=dSum+dx
-
-    if dSum > dSum_lim:
-        dSum = dSum_lim
-    elif dSum < -dSum_lim:
-        dSum = -dSum_lim
-
-    y=dx*0+dSum*0.1
-
-    if y>y_max:
-        y=y_max
-    elif y<-y_max:
-        y=-y_max
-
-
-
-    vel=vel+a*dt*y
-    speed.append(vel)
-
-    if vel > v_max:
-        vel = v_max
-    elif vel < -v_max:
-        vel = -v_max
-
-    x=x+vel*dt
-
+    vel = 10 * y
+    x = x + vel * dt + a * dt * dt / 2
     f.append(x)
-    t.append(i)
+    t.append(timer)
+# end for
 gr.printgr(t,ar,f)
-gr.printgr1(speed)
-
